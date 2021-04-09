f1625058bc1ea2b381b09cab9edc54c3bb5635b1
conr <conr@itu.dk>
2021-04-09 09:15:37 +0200

    Moving average n now 5 instead of 4

--- methods.py
+++ methods.py
@@ -448,7 +448,11 @@ def scoreEvolution(tweetsDict):
     count = 0
     for score in dateArray:
         if score != 0.0:
-            if count >= 3:
+            if count >= 4:
+                Xnorm = (score - Xmin) / (Xmax - Xmin)
+                movAvg = (Xnorm + dateArray[count-1][0] + dateArray[count-2][0] + dateArray[count-3][0] + dateArray[count-4][0]) / 5
+                dateArray[count] = [movAvg, count+1]
+            elif count >= 3:
                 Xnorm = (score - Xmin) / (Xmax - Xmin)
                 movAvg = (Xnorm + dateArray[count-1][0] + dateArray[count-2][0] + dateArray[count-3][0]) / 4
                 dateArray[count] = [movAvg, count+1]
