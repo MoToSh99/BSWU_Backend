69f0138fa654f8738754c7866bbd3bef0eb636e1
Toheed Sharif <info@toheed.dk>
2021-04-16 10:52:46 +0200

    host added

--- app.py
+++ app.py
@@ -47,4 +47,4 @@ def checkData():
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run(host='0.0.0.0', threaded=True, port=5000)
